68a51f697b93497f6cb1ddc616fa1a897321b14a
Brian Chebon <brynnlamura@gmail.com>
2023-10-11 18:22:46 +0300

    removed h2 in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <main>
       <h1>Hello!, welcome to next</h1>
-      <h2>Welcome to Next webpage</h2>
 
       <Link href="/users">Users</Link>
     </main>
